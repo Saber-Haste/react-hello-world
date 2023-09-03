1ddedce8fa74d193c477b1c59040c7b91dd55629
Saber-Haste <binoychullikkal@protonmail.com>
2023-09-03 22:57:36 +0530

    updates

--- App.jsx
+++ App.jsx
@@ -2,21 +2,26 @@ import React from "react"
 import { Text,View,SafeAreaView,StyleSheet } from "react-native"
 const MyComp = () => {
     return (
-        <SafeAreaView style={styles.container}>
-        <View style={styles.container}>
-            <Text>
-                Current Weather
-            </Text>
-        </View>
+        <SafeAreaView style={styles.wrapper}>
+            <View style={styles.container}>
+                <Text>
+                    Current Weather
+                </Text>
+            </View>
         </SafeAreaView>
 
     )
     
 }
-
 const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1
+    },
     container: {
-        backgroundColor: "pink"
-    }
+        backgroundColor: "blue",
+        flex: 1,
+        alignItems: "center"
+    },
+
 })
 export default MyComp
